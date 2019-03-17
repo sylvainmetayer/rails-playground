784c2cc5919df8f7d6a8dfb95ec3ee7af12d4b98
Sylvain METAYER <contact@sylvainmetayer.fr>
2019-03-17 11:37:02 +0100

    cateory

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,18 +3,19 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.includes(:category).all
   end
 
   def show
   end
 
   def edit
+    @categories = Category.all
   end
 
   def update
     puts params.inspect
-    @post.update(params.require(:post).permit(:name, :content))
+    @post.update(params.require(:post).permit(:name, :content, :category_id))
     redirect_to posts_url, notice: 'Article sauvegardé !'
   end
 
@@ -24,7 +25,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new
-    @post.update(params.require(:post).permit(:name, :content))
+    @post.update(params.require(:post).permit(:name, :content, :category_id))
     redirect_to posts_url, notice: 'Article créé !'
   end
 
@@ -35,9 +36,9 @@ class PostsController < ApplicationController
 
   private
 
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   validates :name, presence: true
+  validates :category, presence: true
   belongs_to :category
 end
